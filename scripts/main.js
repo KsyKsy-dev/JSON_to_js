7bd6177b19a2a230eee8fb75744a254bff556e48
KsyKsy-dev <ksy.codedev@gmail.com>
2023-04-17 16:35:29 +0200

    apdate

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,51 @@
+let data = [];
+
 const getData = async function () {
   const response = await fetch("/json/fields.json");
-  setData(await response.json());
+  const data = await response.json();
+  const fieldsId = data.map((arr) => arr.fieldId);
+  console.log(fieldsId);
 };
-function setData(arr) {
-  console.log(arr, "check");
-}
+
 getData();
-setData();
+// #1 retrive value fiedId
+
+const myObj = [
+  {
+    first: "o",
+    second: "n",
+  },
+  {
+    first: "a",
+    second: "b",
+  },
+  {
+    first: "o",
+    second: "m",
+  },
+  {
+    first: "a",
+    second: "d",
+  },
+  {
+    first: "k",
+    second: "i",
+  },
+  {
+    first: "o",
+    second: "l",
+  },
+];
+let repeatList = [];
+const firstV = myObj.map((obj) => obj.first);
+console.log(firstV);
+const repeat = function () {
+  for (let i = 0; i < firstV.length; i++) {
+    console.log(firstV[i]);
+    for (let n = 0; n < firstV.length; i++) {
+      if (firstV[i] === firstV[n]) repeatList.push(firstV[i]);
+    }
+  }
+  console.log(repeatList);
+};
+repeat();
