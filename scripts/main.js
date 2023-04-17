799a8ff489dd22ccf294db89d54fb80843af9dd8
KsyKsy-dev <ksy.codedev@gmail.com>
2023-04-17 17:19:22 +0200

    update

--- scripts/main.js
+++ scripts/main.js
@@ -4,48 +4,19 @@ const getData = async function () {
   const response = await fetch("/json/fields.json");
   const data = await response.json();
   const fieldsId = data.map((arr) => arr.fieldId);
-  console.log(fieldsId);
+
+  let repeatFields = [];
+  const checkFields = function () {
+    for (let i = 0; i < fieldsId.length - 1; i++) {
+      for (let n = 0; n < fieldsId.lenght + 1; i++) {
+        if (firldsId[i] === fieldsId[n]) {
+          repeatFields.push(firstV[i]);
+        }
+      }
+    }
+    console.log(repeatFields);
+  };
+  checkFields();
 };
 
 getData();
-// #1 retrive value fiedId
-
-const myObj = [
-  {
-    first: "o",
-    second: "n",
-  },
-  {
-    first: "a",
-    second: "b",
-  },
-  {
-    first: "o",
-    second: "m",
-  },
-  {
-    first: "a",
-    second: "d",
-  },
-  {
-    first: "k",
-    second: "i",
-  },
-  {
-    first: "o",
-    second: "l",
-  },
-];
-let repeatList = [];
-const firstV = myObj.map((obj) => obj.first);
-console.log(firstV);
-const repeat = function () {
-  for (let i = 0; i < firstV.length; i++) {
-    console.log(firstV[i]);
-    for (let n = 0; n < firstV.length; i++) {
-      if (firstV[i] === firstV[n]) repeatList.push(firstV[i]);
-    }
-  }
-  console.log(repeatList);
-};
-repeat();
